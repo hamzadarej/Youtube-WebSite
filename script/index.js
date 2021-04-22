d5a5eab790390b3639fc5264b42f9f0d33b8234e
hamza <darejhamza@gmail.com>
2021-04-22 12:55:15 +0200

    Updates

---
+++ script/index.js
@@ -0,0 +1,21 @@
+let clicked = false;
+document.querySelector("#home").addEventListener("click", () => {
+  clicked = !clicked;
+  console.log(clicked);
+  if (clicked) {
+    document.querySelector(".left-side").style.cssText = "display:flex;";
+  } else {
+    document.querySelector(".left-side").style.cssText = "display:none ;";
+  }
+});
+const theme = () => {
+  const white =document.querySelector(".white-theme").checked;
+  body = document.querySelector("body").style;
+  console.log(white);
+  if(white) {
+    body.backgroundColor = "black";
+
+  }else {
+    body.backgroundColor="white";
+  }
+}
