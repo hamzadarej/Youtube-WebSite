4557cefbe3baf81e9018bff8273707c1baf3f280
hamza <darejhamza@gmail.com>
2021-04-22 13:56:12 +0200

    youtube-webpage

--- src/script/index.js
+++ src/script/index.js
@@ -11,11 +11,25 @@ document.querySelector("#home").addEventListener("click", () => {
 const theme = () => {
   const white =document.querySelector(".white-theme").checked;
   body = document.querySelector("body").style;
+  let left = document.querySelector(".left-side").style;
+  let nav = document.querySelector(".topnav").style;
+  let container =document.querySelectorAll(".main-container").style;
+  let navMain =document.querySelector(".nav-main").style;
+  let proFil =document.querySelector(".proFil").style;
+  
   console.log(white);
   if(white) {
-    body.backgroundColor = "black";
+    body.backgroundColor ="black";
 
   }else {
     body.backgroundColor="white";
+    left.backgroundColor="transparent";
+    nav.backgroundColor="transparent";
+    navMain.backgroundColor="white";
+    proFil.backgroundColor="white";
+    container.backgroundColo="white";
+
+    
+
   }
 }
