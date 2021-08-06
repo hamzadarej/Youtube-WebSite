f5f3b9d15484f7d97e006300a669d851b4f5776d
hamza <darejhamza@gmail.com>
2021-08-06 15:17:32 +0200

    Updates

--- script/index.js
+++ script/index.js
@@ -1,43 +1,41 @@
 let clicked = false;
 document.querySelector("#home").addEventListener("click", () => {
   clicked = !clicked;
-  console.log(clicked);
+
   if (clicked) {
     document.querySelector(".left-side").style.cssText = "display:flex;";
   } else {
     document.querySelector(".left-side").style.cssText = "display:none ;";
   }
 });
-let click = false;
+let click = true;
 document.querySelector("#user-pic").addEventListener("click", () => {
-  click = !click;
-  console.log(clicked);
   if (click) {
     document.querySelector(".change-color").style.cssText = "display:block;";
   } else {
-    document.querySelector(".change-color").style.cssText = "display:none ;";
+    document.querySelector(".change-color").style.cssText = "display:none;";
   }
 });
 const theme = () => {
-  const white =document.querySelector(".white-theme").checked;
+  const white = document.querySelector(".white-theme").checked;
   let body = document.querySelector("body").style;
   let left = document.querySelector(".left-side").style;
   let nav = document.querySelector(".topnav").style;
-  let container =document.querySelectorAll(".main-container").style;
-  let navMain =document.querySelector(".nav-main").style;
-  let proFil =document.querySelector(".proFil").style;
-  let appA = document.querySelector(".app").style;
-  
-  console.log(white);
-  if(white) {
-    body.backgroundColor ="black";
+  let container = document.querySelectorAll(".main-container").style;
+  let navMain = document.querySelector(".nav-main").style;
+  let proFil = document.querySelector(".proFil").style;
+  let modesColor = document.querySelector(".change-color").style;
 
-  }else {
-    body.backgroundColor="white";
-    left.backgroundColor="transparent";
-    nav.backgroundColor="transparent";
-    navMain.backgroundColor="transparent";
-    proFil.backgroundColor="transparent";
-    container.backgroundColor="transparent";
-    }
-}
+  console.log(white);
+  if (white) {
+    body.backgroundColor = "black";
+  } else {
+    modesColor.backgroundColor = "white";
+    body.backgroundColor = "white";
+    left.backgroundColor = "transparent";
+    nav.backgroundColor = "transparent";
+    navMain.backgroundColor = "transparent";
+    proFil.backgroundColor = "transparent";
+    container.backgroundColor = "transparent";
+  }
+};
